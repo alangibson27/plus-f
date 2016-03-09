66fa9b1d2120574e09631db80329d6df39b2bd5a
Alan Gibson <alangibson27@gmail.com>
2016-03-09 21:58:13 +0000

    Added basic ULA implementation with keyboard handling.

--- src/main/java/com/socialthingy/qaopm/spectrum/Computer.java
+++ src/main/java/com/socialthingy/qaopm/spectrum/Computer.java
@@ -4,12 +4,17 @@ import com.socialthingy.qaopm.snapshot.SnapshotLoader;
 import com.socialthingy.qaopm.z80.*;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -17,12 +22,13 @@ import java.util.concurrent.TimeUnit;
 public class Computer extends Application implements InterruptingDevice {
 
     private final Processor processor;
-    private final IO io;
+    private final ULA ula;
     private final int[] memory;
     private final Display display;
     public static final int T_STATES_PER_REFRESH = 80000;
     private ImageView imageView;
     private PrintWriter dumpFile;
+    private final Map<KeyCode, Character> spectrumKeys = new HashMap<>();
 
     public static void main(final String[] args) throws IOException {
         Application.launch(args);
@@ -30,16 +36,58 @@ public class Computer extends Application implements InterruptingDevice {
 
     public Computer() throws IOException {
         memory = new int[0x10000];
-        io = new DummyIO();
-        processor = new Processor(memory, io);
+        ula = new ULA();
+        processor = new Processor(memory, ula);
         display = new Display(memory);
         dumpFile = new PrintWriter(new FileWriter("/var/tmp/java_spectrum.dump"));
+
+        spectrumKeys.put(KeyCode.A, 'a');
+        spectrumKeys.put(KeyCode.B, 'b');
+        spectrumKeys.put(KeyCode.C, 'c');
+        spectrumKeys.put(KeyCode.D, 'd');
+        spectrumKeys.put(KeyCode.E, 'e');
+        spectrumKeys.put(KeyCode.F, 'f');
+        spectrumKeys.put(KeyCode.G, 'g');
+        spectrumKeys.put(KeyCode.H, 'h');
+        spectrumKeys.put(KeyCode.I, 'i');
+        spectrumKeys.put(KeyCode.J, 'j');
+        spectrumKeys.put(KeyCode.K, 'k');
+        spectrumKeys.put(KeyCode.L, 'l');
+        spectrumKeys.put(KeyCode.M, 'm');
+        spectrumKeys.put(KeyCode.N, 'n');
+        spectrumKeys.put(KeyCode.O, 'o');
+        spectrumKeys.put(KeyCode.P, 'p');
+        spectrumKeys.put(KeyCode.Q, 'q');
+        spectrumKeys.put(KeyCode.R, 'r');
+        spectrumKeys.put(KeyCode.S, 's');
+        spectrumKeys.put(KeyCode.T, 't');
+        spectrumKeys.put(KeyCode.U, 'u');
+        spectrumKeys.put(KeyCode.V, 'v');
+        spectrumKeys.put(KeyCode.W, 'w');
+        spectrumKeys.put(KeyCode.X, 'x');
+        spectrumKeys.put(KeyCode.Y, 'y');
+        spectrumKeys.put(KeyCode.Z, 'z');
+
+        spectrumKeys.put(KeyCode.DIGIT0, '0');
+        spectrumKeys.put(KeyCode.DIGIT1, '1');
+        spectrumKeys.put(KeyCode.DIGIT2, '2');
+        spectrumKeys.put(KeyCode.DIGIT3, '3');
+        spectrumKeys.put(KeyCode.DIGIT4, '4');
+        spectrumKeys.put(KeyCode.DIGIT5, '5');
+        spectrumKeys.put(KeyCode.DIGIT6, '6');
+        spectrumKeys.put(KeyCode.DIGIT7, '7');
+        spectrumKeys.put(KeyCode.DIGIT8, '8');
+        spectrumKeys.put(KeyCode.DIGIT9, '9');
+
+        spectrumKeys.put(KeyCode.SHIFT, '^');
+        spectrumKeys.put(KeyCode.CONTROL, '$');
+        spectrumKeys.put(KeyCode.SPACE, ' ');
+        spectrumKeys.put(KeyCode.ENTER, '_');
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         final String romFile = getParameters().getRaw().get(0);
-        final String snapshotFile = getParameters().getRaw().get(1);
 
         try (final FileInputStream fis = new FileInputStream(romFile)) {
             int addr = 0;
@@ -48,7 +96,10 @@ public class Computer extends Application implements InterruptingDevice {
             }
         }
 
-        loadSnapshot(snapshotFile);
+        if (getParameters().getRaw().size() > 1) {
+            final String snapshotFile = getParameters().getRaw().get(1);
+            loadSnapshot(snapshotFile);
+        }
 
         Group root = new Group();
         Scene scene = new Scene(root);
@@ -58,8 +109,27 @@ public class Computer extends Application implements InterruptingDevice {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        final SpectrumKeyHandler keyHandler = new SpectrumKeyHandler();
+        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, keyHandler);
+        primaryStage.addEventHandler(KeyEvent.KEY_RELEASED, keyHandler);
+
         final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleAtFixedRate(this::singleRefresh, 0, 40, TimeUnit.MILLISECONDS);
+    }
+
+    private class SpectrumKeyHandler implements EventHandler<KeyEvent> {
+
+        @Override
+        public void handle(KeyEvent event) {
+            final Character spectrumKey = spectrumKeys.get(event.getCode());
+            if (spectrumKey != null) {
+                if (event.getEventType() == KeyEvent.KEY_PRESSED) {
+                    ula.keyDown(spectrumKey);
+                } else if (event.getEventType() == KeyEvent.KEY_RELEASED) {
+                    ula.keyUp(spectrumKey);
+                }
+            }
+        }
     }
 
     private void loadSnapshot(String snapshotFile) throws IOException {
@@ -114,16 +184,5 @@ public class Computer extends Application implements InterruptingDevice {
 
     @Override
     public void acknowledge() {
-    }
-}
-
-class DummyIO implements IO {
-    @Override
-    public int read(int port, int accumulator) {
-        return 191;
-    }
-
-    @Override
-    public void write(int port, int accumulator, int value) {
     }
 }
---
+++ src/main/java/com/socialthingy/qaopm/spectrum/ULA.java
@@ -0,0 +1,81 @@
+package com.socialthingy.qaopm.spectrum;
+
+import com.socialthingy.qaopm.z80.IO;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ULA implements IO {
+
+    private static final int[] KEY_POSITIONS = new int[] {
+        binary("11110"),
+        binary("11101"),
+        binary("11011"),
+        binary("10111"),
+        binary("01111")
+    };
+
+    private Set<Character> keysDown = new HashSet<>();
+    private Map<Character, Integer>[] halfRows = new Map[8];
+
+    public ULA() {
+        halfRows[0] = buildHalfRow("^zxcv");
+        halfRows[1] = buildHalfRow("asdfg");
+        halfRows[2] = buildHalfRow("qwert");
+        halfRows[3] = buildHalfRow("12345");
+        halfRows[4] = buildHalfRow("09876");
+        halfRows[5] = buildHalfRow("poiuy");
+        halfRows[6] = buildHalfRow("_lkjh");
+        halfRows[7] = buildHalfRow(" $mnb");
+    }
+
+    private Map<Character, Integer> buildHalfRow(final String rowKeys) {
+        final Map<Character, Integer> keyBits = new HashMap<>();
+        for (int i = 0; i < KEY_POSITIONS.length; i++) {
+            keyBits.put(rowKeys.charAt(i), KEY_POSITIONS[i]);
+        }
+        return keyBits;
+    }
+
+    @Override
+    public int read(int port, int accumulator) {
+        if (port == 0xfe) {
+            if (keysDown.isEmpty()) {
+                return binary("10111111");
+            } else {
+                int bits = binary("11111");
+                for (int i = 0; i < 8; i++) {
+                    if ((accumulator & (1 << i)) == 0) {
+                        for (Character key : keysDown) {
+                            if (halfRows[i].containsKey(key)) {
+                                bits &= halfRows[i].get(key);
+                            }
+                        }
+                    }
+                }
+
+                return binary("10100000") | bits;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void write(int port, int accumulator, int value) {
+
+    }
+
+    public void keyDown(final char key) {
+        keysDown.add(Character.toLowerCase(key));
+    }
+
+    public void keyUp(final char key) {
+        keysDown.remove(Character.toLowerCase(key));
+    }
+
+    private static int binary(final String value) {
+        return Integer.valueOf(value, 2);
+    }
+}
