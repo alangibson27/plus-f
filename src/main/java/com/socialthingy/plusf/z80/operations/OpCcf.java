06e749fa9690615dbe05075360f95b928cd5c2a5
Alan Gibson <alangibson27@gmail.com>
2016-07-01 11:50:27 +0100

    Setting undocumented flags on scf and ccf.

--- src/main/java/com/socialthingy/plusf/z80/operations/OpCcf.java
+++ src/main/java/com/socialthingy/plusf/z80/operations/OpCcf.java
@@ -3,12 +3,15 @@ package com.socialthingy.plusf.z80.operations;
 import com.socialthingy.plusf.z80.FlagsRegister;
 import com.socialthingy.plusf.z80.Operation;
 import com.socialthingy.plusf.z80.Processor;
+import com.socialthingy.plusf.z80.Register;
 
 public class OpCcf implements Operation {
     private final FlagsRegister flagsRegister;
+    private final Register accumulator;
 
     public OpCcf(final Processor processor) {
         this.flagsRegister = processor.flagsRegister();
+        this.accumulator = processor.register("a");
     }
 
     @Override
@@ -17,6 +20,7 @@ public class OpCcf implements Operation {
         flagsRegister.set(FlagsRegister.Flag.H, oldCarry);
         flagsRegister.set(FlagsRegister.Flag.N, false);
         flagsRegister.set(FlagsRegister.Flag.C, !oldCarry);
+        flagsRegister.setUndocumentedFlagsFromValue(accumulator.get());
         return 4;
     }
 
