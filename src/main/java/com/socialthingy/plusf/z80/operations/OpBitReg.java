4f5557b4f7d8bd68047a94aa47020f8b6c631b17
Alan Gibson <alangibson27@gmail.com>
2016-07-03 17:46:11 +0100

    Fixed bugs in refresh register increment for interrupt servicing and bitwise IY operations.

--- src/main/java/com/socialthingy/plusf/z80/operations/OpBitReg.java
+++ src/main/java/com/socialthingy/plusf/z80/operations/OpBitReg.java
@@ -19,7 +19,6 @@ public class OpBitReg extends BitOperation {
     @Override
     public int execute() {
         final boolean bitSet = checkBit(register.get());
-        flagsRegister.set(FlagsRegister.Flag.P, flagsRegister.get(FlagsRegister.Flag.Z));
         flagsRegister.set(
             FlagsRegister.Flag.S,
             (bitPosition == 7 || bitPosition == 5 || bitPosition == 3) && bitSet
