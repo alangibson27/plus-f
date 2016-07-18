39f1e99de4b230fddfa2b8dd5708d04215116fa1
Alan Gibson <alangibson27@gmail.com>
2016-07-18 21:33:51 +0100

    Test cleanup

--- src/test/scala/com/socialthingy/plusf/tape/VariableSpeedBlockSpec.scala
+++ src/test/scala/com/socialthingy/plusf/tape/VariableSpeedBlockSpec.scala
@@ -5,8 +5,6 @@ import java.time.Duration
 
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.collection.JavaConverters._
-
 class VariableSpeedBlockSpec extends FlatSpec with Matchers with TapeMatchers {
   "a variable speed block with an odd number of pilot tones" should "play correctly" in {
     val variableSpeedBlock = new VariableSpeedBlock(Duration.ofMillis(10), Array(0x84), 10, 6, 5, 5, 10, 3, 8)
@@ -23,24 +21,20 @@ class VariableSpeedBlockSpec extends FlatSpec with Matchers with TapeMatchers {
   }
 
   private def checkSameContent(variableSpeedBlock: VariableSpeedBlock, referenceBlock: ReferenceVariableSpeedBlock): Unit = {
-    val newBits = variableSpeedBlock.bits(new SignalState(false)).asScala.toList
+    val newBits = variableSpeedBlock.bits(new SignalState(false))
 
     val referenceTape = new RepeatingList[JBoolean]()
     referenceBlock.write(referenceTape, false)
-    val referenceBits = referenceTape.iterator().asScala.toList
+    val referenceBits = referenceTape.iterator()
 
-    referenceBits.size shouldBe newBits.size
-    referenceBits.indices foreach { idx =>
-      if (idx % 500 == 0) {
-        print(".")
-      }
-      withClue(s"bit $idx (${referenceBits(idx)})") {
-        referenceBits(idx) shouldBe newBits(idx)
-      }
+    while (newBits.hasNext && referenceBits.hasNext) {
+      val newBit = newBits.next
+      val refBit = referenceBits.next
+
+      newBit shouldBe refBit
     }
-  }
 
-  implicit class IntOps(n: Int) {
-    def times(action: => Unit) = (0 until n) foreach (_ => action)
+    newBits.hasNext shouldBe false
+    referenceBits.hasNext shouldBe false
   }
 }
