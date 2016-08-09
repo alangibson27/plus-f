ef7536d4d3b8bcfdc2542a7bca9b699e756adcbc
Alan Gibson <alangibson27@gmail.com>
2016-08-09 20:34:45 +0100

    Precalculating source and target pixel indexes.

--- src/main/java/com/socialthingy/plusf/spectrum/display/JavaFXDoubleSizeDisplay.java
+++ src/main/java/com/socialthingy/plusf/spectrum/display/JavaFXDoubleSizeDisplay.java
@@ -15,6 +15,47 @@ public class JavaFXDoubleSizeDisplay extends Display {
     private static final int DISPLAY_HEIGHT = SCREEN_HEIGHT + (BORDER * 2);
     private static final int SCALE = 2;
 
+    private static final int[][][] SRC_PIXEL_MATRIX = new int[SCREEN_WIDTH][SCREEN_HEIGHT][];
+    static {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            for (int y = 0; y < SCREEN_HEIGHT; y++) {
+                SRC_PIXEL_MATRIX[x][y] = new int[9];
+                SRC_PIXEL_MATRIX[x][y][0] = sourcePixelAt(x - 1, y - 1);
+                SRC_PIXEL_MATRIX[x][y][1] = sourcePixelAt(x, y - 1);
+                SRC_PIXEL_MATRIX[x][y][2] = sourcePixelAt(x + 1, y - 1);
+
+                SRC_PIXEL_MATRIX[x][y][3] = sourcePixelAt(x - 1, y);
+                SRC_PIXEL_MATRIX[x][y][4] = sourcePixelAt(x, y);
+                SRC_PIXEL_MATRIX[x][y][5] = sourcePixelAt(x + 1, y);
+
+                SRC_PIXEL_MATRIX[x][y][6] = sourcePixelAt(x - 1, y + 1);
+                SRC_PIXEL_MATRIX[x][y][7] = sourcePixelAt(x, y + 1);
+                SRC_PIXEL_MATRIX[x][y][8] = sourcePixelAt(x + 1, y + 1);
+            }
+        }
+    }
+
+    private static int sourcePixelAt(final int x, final int y) {
+        return (x & 0xff) + ((y & 0xff) * SCREEN_WIDTH);
+    }
+
+    private static final int[][][] TGT_PIXEL_MATRIX = new int[SCREEN_WIDTH][SCREEN_HEIGHT][];
+    static {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            for (int y = 0; y < SCREEN_HEIGHT; y++) {
+                TGT_PIXEL_MATRIX[x][y] = new int[4];
+                TGT_PIXEL_MATRIX[x][y][0] = targetPixelAt(x * 2, y * 2, 0, 0);
+                TGT_PIXEL_MATRIX[x][y][1] = targetPixelAt(x * 2, y * 2, 1, 0);
+                TGT_PIXEL_MATRIX[x][y][2] = targetPixelAt(x * 2, y * 2, 0, 1);
+                TGT_PIXEL_MATRIX[x][y][3] = targetPixelAt(x * 2, y * 2, 1, 1);
+            }
+        }
+    }
+
+    private static int targetPixelAt(final int mainx, final int mainy, final int subx, final int suby) {
+        return (mainx + subx) + ((mainy + suby) * (SCREEN_WIDTH * SCALE));
+    }
+
     private final WritableImage border = new WritableImage(1, DISPLAY_HEIGHT);
     private final PixelWriter borderWriter = border.getPixelWriter();
     private final PixelWriter screenWriter;
@@ -64,11 +105,11 @@ public class JavaFXDoubleSizeDisplay extends Display {
     private void scale() {
         for (int x = 0; x < SCREEN_WIDTH; x++) {
             for (int y = 0; y < SCREEN_HEIGHT; y++) {
-                final int b = sourcePixels[sourcePixelAt(x, y - 1)];
-                final int d = sourcePixels[sourcePixelAt(x - 1, y)];
-                final int e = sourcePixels[sourcePixelAt(x, y)];
-                final int f = sourcePixels[sourcePixelAt(x + 1, y)];
-                final int h = sourcePixels[sourcePixelAt(x, y + 1)];
+                final int b = sourcePixels[SRC_PIXEL_MATRIX[x][y][1]];
+                final int d = sourcePixels[SRC_PIXEL_MATRIX[x][y][3]];
+                final int e = sourcePixels[SRC_PIXEL_MATRIX[x][y][4]];
+                final int f = sourcePixels[SRC_PIXEL_MATRIX[x][y][5]];
+                final int h = sourcePixels[SRC_PIXEL_MATRIX[x][y][7]];
 
                 final int e0;
                 final int e1;
@@ -86,19 +127,11 @@ public class JavaFXDoubleSizeDisplay extends Display {
                     e3 = e;
                 }
 
-                targetPixels[targetPixelAt(x * 2, y * 2, 0, 0)] = e0;
-                targetPixels[targetPixelAt(x * 2, y * 2, 1, 0)] = e1;
-                targetPixels[targetPixelAt(x * 2, y * 2, 0, 1)] = e2;
-                targetPixels[targetPixelAt(x * 2, y * 2, 1, 1)] = e3;
+                targetPixels[TGT_PIXEL_MATRIX[x][y][0]] = e0;
+                targetPixels[TGT_PIXEL_MATRIX[x][y][1]] = e1;
+                targetPixels[TGT_PIXEL_MATRIX[x][y][2]] = e2;
+                targetPixels[TGT_PIXEL_MATRIX[x][y][3]] = e3;
             }
         }
-    }
-
-    private final int sourcePixelAt(final int x, final int y) {
-        return (x & 0xff) + ((y & 0xff) * SCREEN_WIDTH);
-    }
-
-    private final int targetPixelAt(final int mainx, final int mainy, final int subx, final int suby) {
-        return (mainx + subx) + ((mainy + suby) * scaledWidth);
     }
 }
