3bc46f8a5b07b2924d951af639bf1a4a6937eca4
Alan Gibson <alangibson27@gmail.com>
2016-11-22 21:51:09 +0000

    Added icons to launcher window.

--- src/main/java/com/socialthingy/plusf/spectrum/ui/PlusF.java
+++ src/main/java/com/socialthingy/plusf/spectrum/ui/PlusF.java
@@ -7,29 +7,41 @@ import static java.awt.GridBagConstraints.*;
 
 public class PlusF extends JFrame {
     public PlusF() {
-        final JButton hostButton = new JButton("Host");
+        final JButton hostButton = new JButton(new ImageIcon(getClass().getResource("/icons/host.png")));
         hostButton.addActionListener(l -> start(new Emulator()));
-        final JButton guestButton = new JButton("Guest");
+        hostButton.setFocusPainted(false);
+        final JLabel hostLabel = new JLabel("<html><b>Host</b><br/>For 1-player games, or starting a 2-player session</html>");
+        final JButton guestButton = new JButton(new ImageIcon(getClass().getResource("/icons/guest.png")));
         guestButton.addActionListener(l -> start(new Guest()));
+        guestButton.setFocusPainted(false);
+        final JLabel guestLabel = new JLabel("<html><b>Guest</b><br/>For joining in a 2-player game</html>");
 
-        final Insets insets = new Insets(2, 2, 2, 2);
+        final Insets insets = new Insets(5, 5, 5, 5);
         final Container contentPane = getContentPane();
         contentPane.setLayout(new GridBagLayout());
         contentPane.add(
-                new JLabel("Welcome to Plus-F. Please select which screen to run."),
-                new GridBagConstraints(0, 0, 3, 1, 1.0, 0.0, CENTER, BOTH, insets, 0, 0)
+                new JLabel("<html><span style='font-size:32px'>Welcome to Plus-F!</span><html>"),
+                new GridBagConstraints(0, 0, 4, 1, 1.0, 0.0, CENTER, VERTICAL, insets, 0, 0)
         );
 
-        contentPane.add(new JPanel(), new GridBagConstraints(0, 1, 1, 2, 1.0, 0.0, WEST, HORIZONTAL, insets, 0, 0));
-        contentPane.add(hostButton, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, CENTER, HORIZONTAL, insets, 0, 0));
-        contentPane.add(guestButton, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, CENTER, HORIZONTAL, insets, 0, 0));
-        contentPane.add(new JPanel(), new GridBagConstraints(2, 1, 1, 2, 1.0, 0.0, EAST, HORIZONTAL, insets, 0, 0));
+        contentPane.add(
+                new JLabel("Select which application you wish to run"),
+                new GridBagConstraints(0, 1, 4, 1, 1.0, 0.0, CENTER, VERTICAL, insets, 0, 0)
+        );
+
+        contentPane.add(new JPanel(), new GridBagConstraints(0, 2, 1, 2, 1.0, 0.0, WEST, HORIZONTAL, insets, 0, 0));
+        contentPane.add(hostButton, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, WEST, HORIZONTAL, insets, 0, 0));
+        contentPane.add(hostLabel, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0, CENTER, HORIZONTAL, insets, 0, 0));
+        contentPane.add(guestButton, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0, WEST, HORIZONTAL, insets, 0, 0));
+        contentPane.add(guestLabel, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0, CENTER, HORIZONTAL, insets, 0, 0));
+        contentPane.add(new JPanel(), new GridBagConstraints(3, 2, 1, 2, 1.0, 0.0, EAST, HORIZONTAL, insets, 0, 0));
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
 
         final Rectangle bounds = getGraphicsConfiguration().getBounds();
         setLocation((bounds.width - getWidth()) / 2, (bounds.height - getHeight()) / 2);
+        setTitle("Plus-F");
     }
 
     private void go() {
