54ad17f4d3e1a8991938aa12725bf1b96840ae87
Alan Gibson <alangibson27@gmail.com>
2018-03-19 21:37:51 +0000

    Use DNS name for discovery service.

--- plus-f/release.sh
+++ plus-f/release.sh
@@ -2,8 +2,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${SCRIPT_DIR}/../gradlew clean debPackage rpmPackage
 for i in $(ls build/distributions/plus-f-*); do
-    name=$(echo $i | sed -E "s/(.+plus-f.+)(\....)/Plus-F\2/")
-    echo "Uploading $i to S3 as $name ..."
-    aws s3 cp $i s3://download.socialthingy.com/${name}
-    echo "Done"
+    if ! [[ $i =~ .+tar ]]; then
+      name=$(echo $i | sed -E "s/(.+plus-f.+)(\....)/Plus-F\2/")
+      echo "Uploading $i to S3 as $name ..."
+      aws s3 cp $i s3://download.socialthingy.com/${name}
+      echo "Done"
+    fi
 done
