d4a35c9de658066cc0492c280c79e499ce202341
Alan Gibson <alangibson27@gmail.com>
2018-03-19 21:35:55 +0000

    Bump to v1.5.3, cleaned up release script.

--- plus-f/release.sh
+++ plus-f/release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${SCRIPT_DIR}/../gradlew clean debPackage rpmPackage
-for i in $(ls build/distribution/plus-f-*); do
-    name=$(echo $i | sed -E "s/(plus-f.+)(\....)/Plus-F\2/")
+for i in $(ls build/distributions/plus-f-*); do
+    name=$(echo $i | sed -E "s/(.+plus-f.+)(\....)/Plus-F\2/")
     echo "Uploading $i to S3 as $name ..."
     aws s3 cp $i s3://download.socialthingy.com/${name}
     echo "Done"
