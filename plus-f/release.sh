e58eb298cc45028a77f9bbab28e4f5977efaf53d
Alan Gibson <alangibson27@gmail.com>
2018-12-02 22:33:20 +0000

    Manually build deb and zip
    Use jlink to create a JRE, then package that up with the application.

--- plus-f/release.sh
+++ plus-f/release.sh
@@ -2,16 +2,11 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LAUNCH4J_DIR=${LAUNCH4J_DIR:-/opt/launch4j}
-WINDOWS_JRE_DIR=${WINDOWS_JRE_DIR:-/opt/windows/jre-10.0.2}
-WINDOWS_DIST_DIR=${SCRIPT_DIR}/build/distributions/windows
 AWS_CMD=${AWS_CMD:-aws}
 GRADLE=${SCRIPT_DIR}/../gradlew
-JAVA_HOME=/opt/java/jdk-1.8.0_191
 VERSION=$(${GRADLE} printVersion --quiet)
 
 export SCRIPT_DIR
-export JAVA_HOME
 export VERSION
 
 function writeVersionFile {
@@ -24,7 +19,7 @@ function writeVersionFile {
 
 function checkCommittedOnMaster {
   # Check we are on master
-  if [ $(git rev-parse --abbrev-ref HEAD) = "master" ]; then
+  if [[ $(git rev-parse --abbrev-ref HEAD) = "master" ]]; then
     echo "Current branch is master, proceeding with release"
   else
     echo "Current branch is not master, not releasing"
@@ -35,37 +30,11 @@ function checkCommittedOnMaster {
   git diff-index --quiet HEAD -- || { echo 'uncommitted files found!'; exit 1; }
 }
 
-function buildDistributions {
-  # Common JAR
-  ${GRADLE} clean
-  mkdir ${SCRIPT_DIR}/build
-  ${GRADLE} check shadowJar distZip
-}
-
-function nativePackage {
-    local platform=$1
-
-    ${JAVA_HOME}/bin/javapackager \
-    -deploy \
-    -v \
-    -nosign \
-    -native ${platform} \
-    -outdir ${SCRIPT_DIR}/build/distributions \
-    -outfile Plus-F \
-    -name Plus-F \
-    -description "ZX Spectrum Emulator with Network Play Capability" \
-    -appclass com.socialthingy.plusf.spectrum.ui.PlusF \
-    -srcfiles ${SCRIPT_DIR}/build/libs/plus-f-${VERSION}-all.jar \
-    -BappVersion=${VERSION} \
-    -BlicenseType=MIT \
-    -BmainJar=plus-f-${VERSION}-all.jar
-}
-
 function checkVersionUpdated {
   set +e
   found=$(git tag | grep -c release-${VERSION})
   set -e
-  if [ "$found" != "0" ]; then
+  if [[ "$found" != "0" ]]; then
     echo "Version ${VERSION} already released"
     exit 1
   fi
@@ -78,46 +47,40 @@ function tagRelease {
   git push origin release-${VERSION}
 }
 
-function buildLinuxAndUniversal {
-  # Linux and Universal builds
-  ${GRADLE} distZip -x check
-  nativePackage deb
-  nativePackage rpm
-  for i in $(find build/distributions -name "plus-f-*[zip|deb|rpm]"); do
-    name=$(echo $i | sed -E "s/(.+plus-f.+)(\....)/Plus-F\2/")
-    echo "Uploading $i to S3 as $name ..."
-    ${AWS_CMD} s3 cp $i s3://download.socialthingy.com/${name}
-    echo "Done"
-  done
+function buildDeb {
+  DEB_DIR=${SCRIPT_DIR}/build/deb
+  ${GRADLE} installDist -x check
+  sed -i '$ i JAVACMD=$APP_HOME/runtime/bin/java' ${SCRIPT_DIR}/build/install/plus-f/bin/plus-f
+
+  rm -rf ${DEB_DIR} || true
+  jlink --verbose --no-header-files --no-man-pages --compress=2 --strip-debug \
+        --add-modules java.base,java.desktop,java.logging,java.management,java.naming,java.sql,java.xml,jdk.unsupported \
+        --output ${DEB_DIR}/data/opt/Plus-F/runtime
+
+  mv ${SCRIPT_DIR}/build/install/plus-f/* ${DEB_DIR}/data/opt/Plus-F
+  cp -r ${SCRIPT_DIR}/package/linux/deb/* ${DEB_DIR}
+  echo "Version: ${VERSION}" >> ${DEB_DIR}/control/control
+
+  tar czf build/deb/data.tar.gz -C build/deb/data .
+  tar czf build/deb/control.tar.gz -C build/deb/control .
+
+  ar r ${DEB_DIR}/Plus-F.deb ${DEB_DIR}/debian-binary ${DEB_DIR}/control.tar.gz ${DEB_DIR}/data.tar.gz
+
+  ${AWS_CMD} s3 cp ${DEB_DIR}/Plus-F.deb s3://download.socialthingy.com/Plus-F.deb
 }
 
-function buildWindows {
-  # Windows build
-  mkdir -p ${WINDOWS_DIST_DIR}
-
-  # Create launch4j package
-  ${SCRIPT_DIR}/package/windows/template-launch4j.sh > ${WINDOWS_DIST_DIR}/launch4j.xml
-  ${LAUNCH4J_DIR}/launch4j ${WINDOWS_DIST_DIR}/launch4j.xml
-  cp -rf ${WINDOWS_JRE_DIR} ${WINDOWS_DIST_DIR}
-
-  # Create installer
-  ${SCRIPT_DIR}/package/windows/template-iss.sh > ${WINDOWS_DIST_DIR}/plus-f.iss
-  wine "C:/Program Files (x86)/Inno Setup 5/ISCC.exe" Z:${WINDOWS_DIST_DIR}/plus-f.iss
-  ${AWS_CMD} s3 cp ${WINDOWS_DIST_DIR}/Plus-F-Install.exe s3://download.socialthingy.com/Plus-F.exe
+function buildUniversal {
+  ${GRADLE} distZip -x check
+  ${AWS_CMD} s3 cp ${SCRIPT_DIR}/build/distributions/plus-f-${VERSION}.zip s3://download.socialthingy.com/Plus-F.zip
 }
 
 if [[ ! -e "build" ]]; then
   mkdir build
 fi
 
-if [[ ! -e "lib" ]]; then
-  mkdir lib
-fi
-
 writeVersionFile
 checkCommittedOnMaster
 checkVersionUpdated
-buildDistributions
 tagRelease
-buildLinuxAndUniversal
-buildWindows
+buildDeb
+buildUniversal
