094389631c0b400651866d930bf757551584ee31
Alan Gibson <alangibson27@gmail.com>
2018-12-04 22:06:02 +0000

    Fixed paths in release.bat

--- plus-f/release.bat
+++ plus-f/release.bat
@@ -1,4 +1,4 @@
 call ..\gradlew clean installDist
-call jlink --verbose --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.desktop,java.logging,java.management,java.naming,java.sql,java.xml,jdk.unsupported --output build\install\plus-f\runtime
-call iscc.exe package\windows\plus-f.iss /Obuild /FPlus-F
+call "C:\Program Files\Java\jdk11\bin\jlink.exe" --verbose --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.desktop,java.logging,java.management,java.naming,java.sql,java.xml,jdk.unsupported --output build\install\plus-f\runtime
+call "C:\Program Files (x86)\Inno Setup 5\iscc.exe" package\windows\plus-f.iss /Obuild /FPlus-F
 call aws s3 cp build/Plus-F.exe s3://download.socialthingy.com/Plus-F.exe
