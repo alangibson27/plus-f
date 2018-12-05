a2989055b667e3b4ef3724dc597335cc2bb34a66
Alan Gibson <alangibson27@gmail.com>
2018-12-05 23:16:39 +0000

    Workaround for large JVM

--- plus-f/build.sh
+++ plus-f/build.sh
@@ -59,6 +59,8 @@ function buildDeb {
         --add-modules java.base,java.desktop,java.logging,java.management,java.naming,java.sql,java.xml,jdk.unsupported \
         --output ${DEB_DIR}/data/opt/Plus-F/runtime
 
+  strip -p --strip-unneeded ${DEB_DIR}/data/opt/Plus-F/runtime/lib/server/libjvm.so
+
   mv ${SCRIPT_DIR}/build/install/plus-f/* ${DEB_DIR}/data/opt/Plus-F
   cp -r ${SCRIPT_DIR}/package/linux/deb/* ${DEB_DIR}
   echo "Version: ${VERSION}" >> ${DEB_DIR}/control/control
