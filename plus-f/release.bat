947fa900b10740f913cba4edbabcc8bf67b6355f
Alan Gibson <alangibson27@gmail.com>
2019-03-13 22:42:00 +0000

    Set JAVA_HOME for Windows builds

--- plus-f/release.bat
+++ plus-f/release.bat
@@ -1,3 +1,4 @@
+set JAVA_HOME="C:\Program Files\Java\jdk11"
 call ..\gradlew clean installDist
 call "C:\Program Files\Java\jdk11\bin\jlink.exe" --verbose --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.desktop,java.logging,java.management,java.naming,java.sql,java.xml,jdk.unsupported --output build\install\plus-f\runtime
 call "C:\Program Files (x86)\Inno Setup 5\iscc.exe" package\windows\plus-f.iss /Obuild /FPlus-F
